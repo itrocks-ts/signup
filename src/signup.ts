bbcfad37c7f6f46a6ffe2843e42c2038b6d89b93
Baptiste Pillot <baptiste@pillot.fr>
2025-04-01 09:43:43 +0200

    Type-restricted actions support

--- src/signup.ts
+++ src/signup.ts
@@ -1,26 +1,28 @@
 import { Action }       from '@itrocks/action'
 import { Request }      from '@itrocks/action-request'
+import { Type }         from '@itrocks/class-type'
 import { dataToObject } from '@itrocks/data-to-object'
 import { dataSource }   from '@itrocks/storage'
 import { User }         from '@itrocks/user'
 
-export class Signup extends Action
+export class Signup<T extends User = User> extends Action<T>
 {
 
-	async html(request: Request<User>)
+	async html(request: Request<T>)
 	{
+		const userType: Type<User> = request.type
 		let templateName = 'signup'
-		let user         = new request.type
+		let user         = new userType
 
 		if (Object.keys(request.request.data).length) {
 			await dataToObject(user, request.request.data)
 			const { email, login, password } = user
 			if (email.length && login.length && password.length) {
 				const dao   = dataSource()
-				const found = await dao.searchOne(User, {email})
-					|| await dao.searchOne(User, {login})
-					|| await dao.searchOne(User, {email: login})
-					|| await dao.searchOne(User, {login: email})
+				const found = await dao.searchOne(userType, { email })
+					|| await dao.searchOne(userType, { login })
+					|| await dao.searchOne(userType, { email: login })
+					|| await dao.searchOne(userType, { login: email })
 				if (found) {
 					templateName = 'signup-error'
 					user         = found
